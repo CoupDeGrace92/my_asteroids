5e75740c0889194d03baeeaf287fda143dfccbd8
CoupDeGrace92 <nickoboy1992@gmail.com>
2025-08-13 10:43:39 -0600

    created groups for objects that update and objects that are drawn and added the player to them

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     dt = 0
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     main_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None, special_flags=0)
-        main_player.update(dt)
+        updatable.update(dt)
         main_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+
 #This is the pythonic way to run main() to ensure it only runs when this file is called and not another file
 if __name__ == "__main__":
     main()
