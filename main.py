60b18997bcbcd92d069d838812f84c97f40dfc26
CoupDeGrace92 <nickoboy1992@gmail.com>
2025-08-13 09:19:04 -0600

    Added a circleshape.py and created the CircleShape class, added a frame rate limiter in main.py

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pygame
 from constants import *
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 #This is the pythonic way to run main() to ensure it only runs when this file is called and not another file
 if __name__ == "__main__":
     main()
