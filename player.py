e1bd4500f3f19947c9bf9c41ff789ce00ebb0288
CoupDeGrace92 <nickoboy1992@gmail.com>
2025-08-13 09:32:58 -0600

    Initialized the player class and shape

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player(CircleShape):
+    def __init__(self, x, y, PLAYER_RADIUS):
+        super().__init__(PLAYER_RADIUS)
+        self.__x = x
+        self.__y = y
+        self.rotation = 0
+        self.position = (x,y)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
